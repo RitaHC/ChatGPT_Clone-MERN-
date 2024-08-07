477a8b8c7bfae3c98f1d8867fe465e177f9b7a47
RitaHC <ritahameerchauhan@ritas-air.mynetworksettings.com>
2024-08-06 20:21:15 -0400

    Server file started

---
+++ server.js
@@ -0,0 +1,26 @@
+// import express from "express";
+const express = require("express");
+const morgan = require('morgan');
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const colors = require('colors')
+const dotenv = require('dotenv')
+
+
+// dotenv
+dotenv.config()
+
+// res object
+const app = express()
+
+// Middleware
+app.use(cors())
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(morgan('dev'))
+app.use(express.json())
+
+const PORT = process.env.PORT || 8080
+// listen to the server
+app.listen(PORT, ()=>{
+    console.log(`Server Running in ${process.env.DEV_MODE} on${PORT}`.bgCyan.white)
+})
